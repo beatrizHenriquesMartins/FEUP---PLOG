8e68afa409b6f671eb8310589fbfca934226dfbd
Beatriz Velho <up201700491@fe.up.pt>
2017-10-11 09:15:27 +0100

    Criada pasta com o código implementado até ao momento

--- 1º Entrega Intercalar/Código/Impressao.pl
+++ 1º Entrega Intercalar/Código/Impressao.pl
@@ -1,5 +1,5 @@
-/* Nomenclatura das pe√ßas
-        __________|_Pe√ßas Brancas_|_Pe√ßas Pretas_
+/* Nomenclatura das peÁas
+        __________|_PeÁas_Pretas_|_PeÁas_Brancas
         Rei      |      k        |      kk     |
         Rainha   |      q        |      qq     |
         Torre 1  |      t1       |      tt1    |
@@ -12,10 +12,10 @@
 Outras nomenclaturas:
         vazio - casa do tabuleiro vazia.
 
-Representa√ß√£o do Tabuleiro
+RepresentaÁ„o do Tabuleiro
  ________________________________________________
 |     |     |     |     |     |     |     |     |
-|  k  |  kk | bb2 |     |     |     |     |     |  1
+|     |     |     |     |     |     |     |     |  1
 |_____|_____|_____|_____|_____|_____|_____|_____| 
 |     |     |     |     |     |     |     |     |
 |     |     |     |     |     |     |     |     |  2
@@ -40,8 +40,22 @@ Representa√ß√£o do Tabuleiro
 |_____|_____|_____|_____|_____|_____|_____|_____| 
    a     b     c     d     e     f     g     h
 
+____________________
+|_Pecas_Capturadas_|
+
+@Jogador Branco
+k, q, t1, t2, b1, b2, c1, c2
+
+@Jogador Preto
+kk, qq, tt1, tt2, bb1, bb2, cc1, cc2 
+
 */
 
+% pecasCapturadas(JogadorID, [ListaPecasCapturadas])
+pecasCapturadas(branco,[q, t1, t2, b1, b2, c2]).
+pecasCapturadas(preto,[tt1, bb2, cc1, cc2]).
+
+% Inicializa o tabuleiro com as peÁas na casa correta
 inicializarTabuleiro(
 [
 [vazio,vazio,vazio,vazio,vazio,vazio,vazio,vazio],
@@ -58,22 +72,22 @@ inicializarTabuleiro(
 impimirIdentificadoresColunas:-
         write('   a     b     c     d     e     f     g     h').
 
-% Lista com os n√∫meros que permitem identificar uma linha do tabuleiro
+% Lista com os numeros que permitem identificar uma linha do tabuleiro
 numeroLinhas(['8','7','6','5','4','3','2','1']).
 
 % Imprime o limite superior do tabuleiro
 imprimirSeparadorInicial:-
         write(' _______________________________________________').
 
-% Imprime o separador das linhas do tabuleiro
+% Imprime o separador de linhas do tabuleiro
 imprimirSeparadorLinhas:-
         write('|_____|_____|_____|_____|_____|_____|_____|_____|  ').
 
-% Imprime o separador das colunas do tabuleiro
+% Imprime o separador de colunas do tabuleiro
 imprimirSeparadorColunas:-
         write('|     |     |     |     |     |     |     |     |').
 
-% Imprime uma casa do tabuleira com a peca Peca
+% Imprime uma casa do tabuleiro com a peca "Peca"
 imprimeCasa(Peca, L):-
         L = 1, write('  '), write(Peca), write('  ').
 imprimeCasa(Peca, L):-
@@ -83,29 +97,53 @@ imprimeCasa(Peca, L):-
 imprimeCasa(_, _):-
         write('     ').
         
-% Imprime as pe√ßas que est√£o numa determinada linha do tabuleiro
+% Imprime as pecas que estao numa determinada linha do tabuleiro
 imprimirPecasLinha([]).
 imprimirPecasLinha([H | T]):- 
         atom_length(H, L), write('|'),
         imprimeCasa(H, L),
         imprimirPecasLinha(T).
 
+% Imprime a linha numero "NLinha" do tabuleiro
 imprimirLinha([] , []).
 imprimirLinha(Linha , NLinha):-
         imprimirSeparadorColunas, nl,
         imprimirPecasLinha(Linha), write('|   '), write(NLinha), nl,
         imprimirSeparadorLinhas, nl.
 
+% Imprime todas as linhas do tabuleiro
 imprimirLinhas([],[]). 
 imprimirLinhas([Linha | T], [NLinha | ListaLinhas]):- 
-                imprimirLinha(Linha, NLinha), 
-                imprimirLinhas(T, ListaLinhas).
+        imprimirLinha(Linha, NLinha), 
+        imprimirLinhas(T, ListaLinhas).
 
+% Imprime as pecas capturadas por cada um dos jogadores
+imprimirListaPecasCapturadas([]).
+imprimirListaPecasCapturadas([H | T]):-
+        write(H), write(' '),
+        imprimirListaPecasCapturadas(T).
+
+% Imprime uma lista de pecas capturadas pelo jogador "Jogador"
+imprimirListaPecasCapturadas(Jogador, ListaPecas):-
+        write('- Jogador '), write(Jogador), nl,
+        imprimirListaPecasCapturadas(ListaPecas).
+
+% Imprime as pecas capturadas pelos dois jogadores
+imprimirPecasCapturadas:-
+        pecasCapturadas(branco, Lista1),
+        pecasCapturadas(preto, Lista2), nl,
+        write(' __________________'), nl, write('|_Pecas_Capturadas_|'), nl,
+        imprimirListaPecasCapturadas(branco, Lista1), nl, nl,
+        imprimirListaPecasCapturadas(preto, Lista2).
+        
+% Imprime o tabuleiro com o estado atual do jogo
 imprimirTabuleiro([H | T]):-
-        imprimirSeparadorInicial , nl,
+        imprimirSeparadorInicial, nl,
         numeroLinhas(ListaLinhas),
         imprimirLinhas([H | T], ListaLinhas), nl,
-        impimirIdentificadoresColunas, nl. 
+        impimirIdentificadoresColunas, nl,
+        imprimirPecasCapturadas, nl, nl. 
 
-come√ßarCorridaReis:-
-        inicializarTabuleiro(T), imprimirTabuleiro(T).
+% Inicia o jogo Corrida de Reis
+comeÁarCorridaReis:-
+        inicializarTabuleiro(T), imprimirTabuleiro(T). 
